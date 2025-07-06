693b13779a9c0d60a42e225033acd9ee6d3fe02a
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2025-07-06 21:20:51 +0700

    implement art section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 /**
  * ScrollTrigger is a gsap plugin for scroll (animate things base on scroll position)
@@ -20,6 +21,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 };
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
 import { SplitText } from 'gsap/all';
-import React from 'react'
 
 const About = () => {
     useGSAP(() => {
