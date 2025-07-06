cb5dc3a1c5e137d7462f5e83cc6b1cdb738fa99c
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2025-07-06 20:53:56 +0700

    Implement about section components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 /**
  * ScrollTrigger is a gsap plugin for scroll (animate things base on scroll position)
@@ -18,6 +19,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
